2e2e7ab7bba85ca8b86a588617a57ba15fb741d5
Hypothetical <adi.varshney09@gmail.com>
2025-09-05 16:09:26 -0500

    Day 5 - Code
    Commits containing code for the problem 'Remove Duplicates From Sorted Array'. Day 5.

---
+++ Week 1/9-5-2025/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,13 @@
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int currPos = 0;
+        for (int pointer = 0; pointer<nums.length; pointer++) {
+            if ( nums[pointer] > nums[currPos] ) {
+                currPos++;
+                nums[currPos] = nums[pointer];
+            }
+        }
+
+        return currPos+1;
+    }
+}
