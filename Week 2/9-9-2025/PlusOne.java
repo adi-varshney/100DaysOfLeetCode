7c0e49eb161f372bbde9a4b451252197e51519a1
Hypothetical <adi.varshney09@gmail.com>
2025-09-09 17:37:01 -0500

    Day 9 Code
    Commits containing code on (Problem 66.) Plus One for day 9.

---
+++ Week 2/9-9-2025/PlusOne.java
@@ -0,0 +1,28 @@
+// Plus One
+class Solution {
+    public int[] plusOne(int[] digits) {
+        if (digits[digits.length - 1] != 9) {
+            digits[digits.length - 1]++;
+            return digits;
+        } else {
+            int i = digits.length - 1;
+            while (i >= 0) {
+                if (digits[i] == 9 && i != 0) {
+                    i--;
+                    continue;
+                } else if (digits[i] == 9) {
+                    int[] arr = new int[digits.length + 1];
+                    arr[0] = 1;
+                    return arr;
+                } else {
+                    digits[i]++;
+                    for (int j = i + 1; j < digits.length; j++) {
+                        digits[j] = 0;
+                    }
+                    return digits;
+                }
+            }
+        }
+        return new int[1];
+    }
+}
