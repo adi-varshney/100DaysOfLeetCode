c550134d7a809205aae0cb0b26fc8b69aea8300e
Hypothetical <adi.varshney09@gmail.com>
2025-09-11 19:28:42 -0500

    Day 11 Code
    Commits containing code on (Problem 69.) Sqrt(x) for day 11.

---
+++ Week 2/9-11-2025/Sqrt(x).java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        if ( x == 0 )
+            return 0;
+        if ( x == 1 || x == 2 || x == 3 )
+            return 1;
+
+        int low = 1, high = x/2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (x/mid == mid) {
+                return mid;
+            } else if (mid > x/mid) {
+                high = mid - 1;
+            } else if (mid < x/mid) {
+                low = mid + 1;
+            }
+        }
+
+        return high;
+    }
+}
