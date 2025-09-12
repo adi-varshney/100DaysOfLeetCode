71291f2143d7e0157293e81f2b468b5701f35c97
Hypothetical <adi.varshney09@gmail.com>
2025-09-12 17:34:38 -0500

    Day 12 Code
    Commits containing code on (Problem 83.) Remove Duplicates from Sorted List for day 12.

---
+++ Week 2/9-12-2025/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,62 @@
+
+import java.util.HashSet;
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// My initial solution using a HashSet to track seen values.
+// This works for unsorted lists but uses extra space.
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        ListNode iterator = head, prev = null;
+        while (iterator != null) {
+            if (seen.contains(iterator.val)) {
+                prev.next = iterator.next;
+                iterator = iterator.next;
+            } else {
+                seen.add(iterator.val);
+                prev = iterator;
+                iterator = iterator.next;
+            }
+        }
+
+        return head;
+    }
+}
+
+// Optimized solution that leverages the sorted property of the list.
+// This approach uses O(1) extra space and is more efficient.
+class Solution2 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode iterator = head;
+        while (iterator != null && iterator.next != null) {
+            if (iterator.val == iterator.next.val) {
+                iterator.next = iterator.next.next;
+            } else {
+                iterator = iterator.next;
+            }
+        }
+
+        return head;
+    }
+}
