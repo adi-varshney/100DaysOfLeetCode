9102d8b363349f5a4082983cd30f7c531ddcca94
Hypothetical <adi.varshney09@gmail.com>
2025-09-18 20:43:51 -0500

    Formatting

--- Week 3/9-17-2025/SymmetricTree.java
+++ Week 3/9-17-2025/SymmetricTree.java
@@ -39,32 +39,43 @@ class Solution {
 
     public boolean isSymmetric(TreeNode leftChild, TreeNode rightChild) {
         // handle all potential null and false cases first
-        if ((leftChild.left == null && rightChild.right == null) && (leftChild.right == null && rightChild.left == null)) {
+        if ((leftChild.left == null && rightChild.right == null)
+                && (leftChild.right == null && rightChild.left == null)) {
             return true; // if all null, then return true(no more children to check and it is symmetric)
         }
-        
-        if ((leftChild.left == null && rightChild.right != null) || (leftChild.left != null && rightChild.right == null)) {
-            return false; // if one node in the outer pair is null, but not the other then return false(is not symmetric)
+
+        if ((leftChild.left == null && rightChild.right != null)
+                || (leftChild.left != null && rightChild.right == null)) {
+            return false; // if one node in the outer pair is null, but not the other then return false(is
+                          // not symmetric)
         }
-        
-        if ((rightChild.left == null && leftChild.right != null) || (rightChild.left != null && leftChild.right == null)) {
-            return false; // if one node in the inner pair is null, but not the other then return false(is not symmetric)
+
+        if ((rightChild.left == null && leftChild.right != null)
+                || (rightChild.left != null && leftChild.right == null)) {
+            return false; // if one node in the inner pair is null, but not the other then return false(is
+                          // not symmetric)
         }
 
         if (leftChild.left == null && rightChild.right == null)
             if (rightChild.left.val == leftChild.right.val)
-                return isSymmetric(rightChild.left, leftChild.right); // if one pair is null(outer), and other is true(inner)
+                return isSymmetric(rightChild.left, leftChild.right); // if one pair is null(outer), and other is
+                                                                      // true(inner)
             else
                 return false;
         else if (rightChild.left == null && leftChild.right == null)
             if (leftChild.left.val == rightChild.right.val)
-                return isSymmetric(leftChild.left, rightChild.right); // if one pair is true(outer), and other is null(inner)
+                return isSymmetric(leftChild.left, rightChild.right); // if one pair is true(outer), and other is
+                                                                      // null(inner)
             else
                 return false;
         else if ((leftChild.left.val == rightChild.right.val) && (rightChild.left.val == leftChild.right.val))
-            return isSymmetric(leftChild.left, rightChild.right) && isSymmetric(rightChild.left, leftChild.right); // if all equal each other
-        else 
+            return isSymmetric(leftChild.left, rightChild.right) && isSymmetric(rightChild.left, leftChild.right); // if
+                                                                                                                   // all
+                                                                                                                   // equal
+                                                                                                                   // each
+                                                                                                                   // other
+        else
             return false;
-        
+
     }
 }
