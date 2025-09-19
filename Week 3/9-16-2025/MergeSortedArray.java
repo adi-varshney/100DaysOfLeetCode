423c7b97a84ded0aab9117f86fe90b9fe73f1a5b
Hypothetical <adi.varshney09@gmail.com>
2025-09-18 20:43:36 -0500

    Formatting

--- Week 3/9-16-2025/MergeSortedArray.java
+++ Week 3/9-16-2025/MergeSortedArray.java
@@ -5,21 +5,21 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         if (nums2.length == 0)
             return;
-        
-        int i1 = m-1, i2 = n-1;
-        for (int i = (m+n)-1; i >= 0; i--) {
+
+        int i1 = m - 1, i2 = n - 1;
+        for (int i = (m + n) - 1; i >= 0; i--) {
             if (i1 < 0) {
                 nums1[i] = nums2[i2];
                 i2--;
                 continue;
             }
-            
+
             if (i2 < 0) {
                 nums1[i] = nums1[i1];
                 i1--;
                 continue;
             }
-            
+
             if (nums1[i1] >= nums2[i2]) {
                 nums1[i] = nums1[i1];
                 i1--;
