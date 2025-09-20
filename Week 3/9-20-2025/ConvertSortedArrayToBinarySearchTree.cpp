201559e3b466758bec4ccb4401a9e2a8a5fb879d
Hypothetical <adi.varshney09@gmail.com>
2025-09-20 16:26:47 -0500

    Day 20 C++ Code
    Trying to get more experience with C++! Commits containing code on (Problem 108.) Convert Sorted Array To Binary Search Tree for day 20.

---
+++ Week 3/9-20-2025/ConvertSortedArrayToBinarySearchTree.cpp
@@ -0,0 +1,35 @@
+// 108. Convert Sorted Array to Binary Search Tree
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+
+#include <vector>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *sortedArrayToBST(vector<int> &nums)
+    {
+        return createChildren(nums, 0, nums.size() - 1);
+    }
+
+    TreeNode *createChildren(vector<int> &nums, int start, int end)
+    {
+        if (start > end)
+            return nullptr;
+
+        int middle = start + (end - start) / 2;
+
+        return new TreeNode(nums[middle], createChildren(nums, start, middle - 1), createChildren(nums, middle + 1, end));
+    }
+};
