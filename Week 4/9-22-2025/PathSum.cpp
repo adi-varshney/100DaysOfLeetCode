2b1f85bbe3760dfe95e1003e482df0ed30febc22
Hypothetical <adi.varshney09@gmail.com>
2025-09-22 16:21:38 -0500

    Day 22 Code
    Coded in C++ to get more experience. Commits containing code on (Problem 112.) Path Sum for day 22.

---
+++ Week 4/9-22-2025/PathSum.cpp
@@ -0,0 +1,38 @@
+// 111. Minimum Depth of Binary Tree
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/
+
+#include <queue>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr)
+            return false;
+        
+        return findLeaf(root, 0, targetSum);
+    }
+
+    bool findLeaf(TreeNode* curr, int currSum, int targetSum) {
+        if (curr == nullptr)
+            return false;
+            
+        currSum += curr->val;
+        if (!curr->left && !curr->right)
+            return currSum == targetSum;
+        else
+            return findLeaf(curr->left, currSum, targetSum) || findLeaf(curr->right, currSum, targetSum);
+            
+    }
+};
