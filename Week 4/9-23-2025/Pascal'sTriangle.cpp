8ad979398076ec6a91606edca1ebf36d4dbbb7fc
Hypothetical <adi.varshney09@gmail.com>
2025-09-23 21:37:10 -0500

    Day 23 Code
    Coded in C++ to get more experience. Commits containing code on (Problem 118.) Pascal's Triangle for day 23.

---
+++ Week 4/9-23-2025/Pascal'sTriangle.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> t;
+        t.emplace_back(vector<int> {1});
+        if (numRows == 1)
+            return t;
+        
+        for (int i = 2; i<=numRows; i++) {    
+            t.emplace_back(createRow(t, i));
+        }
+
+        return t;
+    }
+
+    vector<int> createRow(vector<vector<int>>& t, int rowSize) {
+        vector<int> row;
+        row.push_back(1);
+
+        for (int i = 0; i<rowSize-2; i++) {
+            row.push_back(t[rowSize-2][i]+t[rowSize-2][i+1]);
+        }
+
+        row.push_back(1);
+
+        return row;
+    }
+};
