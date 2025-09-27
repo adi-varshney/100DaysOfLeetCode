185cc4e6028e04d36d75cef544b206b51719c433
Hypothetical <adi.varshney09@gmail.com>
2025-09-26 23:41:51 -0500

    Day 26 Code
    Coded in C++ to get more experience. Commits containing code on (Problem 125.) Valid Palindrome for day 26.

---
+++ Week 4/9-26-2025/ValidPalindrome.cpp
@@ -0,0 +1,34 @@
+#include <ctype.h>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int leftPtr = 0, rightPtr = s.size()-1;
+
+        // ascii values: 48('0')-57('9'), 65('A')-91('Z'), and 97('a') - 122('z')
+        while (leftPtr < rightPtr) {
+            char leftLower = tolower(s[leftPtr]);
+            char rightLower = tolower(s[rightPtr]);
+
+            if (!(leftLower >= '0' && leftLower <= '9') && !(leftLower >= 'A' && leftLower <= 'Z') && !(leftLower >= 'a' && leftLower <= 'z')) {
+                leftPtr++;
+                continue;
+            }
+            
+            if (!(rightLower >= '0' && rightLower <= '9') && !(rightLower >= 'A' && rightLower <= 'Z') && !(rightLower >= 'a' && rightLower <= 'z')) {
+                rightPtr--;
+                continue;
+            }
+
+            if (leftLower != rightLower)
+                return false;
+
+            leftPtr++;
+            rightPtr--;
+        }
+
+        return true;
+    }
+};
