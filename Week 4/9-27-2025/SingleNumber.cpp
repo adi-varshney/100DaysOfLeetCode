b2e61749215d33080c28fd83c0ac89ec178e1ca1
Hypothetical <adi.varshney09@gmail.com>
2025-09-27 22:05:36 -0500

    Day 27 Code
    Coded in C++ to get more experience. Commits containing code on (Problem 136.) Single Number for day 27.

---
+++ Week 4/9-27-2025/SingleNumber.cpp
@@ -0,0 +1,15 @@
+// 136. Single Number
+// https://leetcode.com/problems/single-number/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+
+        for (int i: nums) {
+            ans ^= i;
+        }
+
+        return ans;
+    }
+};
