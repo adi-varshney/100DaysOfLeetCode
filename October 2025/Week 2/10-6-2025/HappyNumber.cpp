25781d1c6edb1a08d0b970a3e4d34422f7cadc6e
Hypothetical <adi.varshney09@gmail.com>
2025-10-06 19:45:55 -0500

    Day 36 Code
    Coded in C++ to gain experience. Implements two approaches to solve the Happy Number problem: one using a hash set to detect cycles, and another using pattern observation for single-digit numbers for Day 36.

---
+++ October 2025/Week 2/10-6-2025/HappyNumber.cpp
@@ -0,0 +1,59 @@
+// 202. Happy Number
+// https://leetcode.com/problems/happy-number/
+
+using namespace std;
+
+// Solution 1: HashSet
+#include <unordered_set>
+class Solution1 {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> calculated;
+
+        while (calculated.count(n) != 1) {
+            calculated.insert(n);
+            
+            int ans = 0;
+            while (n != 0) {
+                ans += pow(n % 10, 2);
+                n /= 10;
+            }
+
+            n = ans;
+
+            //cout << n << endl; // testing purposes
+
+            if (log10(n) - (int) log10(n)  == 0.0)
+                return true;
+        }
+        
+        //cout << n << endl; // testing purposes
+
+        return false;
+    }
+};
+
+// Solution 2: Pattern observation
+class Solution2 {
+public:
+    bool isHappy(int n) {
+        while (true) {
+            if (n == 1 || n == 7)
+                return true;
+            else if (n < 10)
+                return false;
+
+            int ans = 0;
+            while (n != 0) {
+                ans += pow(n % 10, 2);
+                n /= 10;
+            }
+
+            n = ans;
+
+            //cout << n << endl; // testing purposes
+        }
+
+        return false;
+    }
+};
