c91a3f5b6a0ed787fe33583bea1c8332becae7eb
Hypothetical <adi.varshney09@gmail.com>
2025-10-09 17:28:00 -0500

    Day 39 Code
    Implements the reverseList function for LeetCode problem 206, which reverses a singly-linked list. Includes a Solution class with the iterative reversal logic for day 39.

---
+++ October 2025/Week 2/10-9-2025/ReverseLinkedList.cpp
@@ -0,0 +1,38 @@
+// 206. Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == nullptr)
+            return head;
+
+        ListNode *prev = nullptr;
+        ListNode *n = head->next;
+        while (n != nullptr)
+        {
+            head->next = prev;
+            prev = head;
+            head = n;
+            n = n->next;
+        }
+
+        prev = new ListNode(head->val, prev);
+
+        return prev;
+    }
+};
