2889a192e5f7320cd0c70d059c1ab28e94c17a68
Hypothetical <adi.varshney09@gmail.com>
2025-10-13 11:45:13 -0500

    Day 43 Code
    Implements LeetCode problem 222 (Count Complete Tree Nodes) using a binary search approach to efficiently count nodes in a complete binary tree. Includes helper function to determine the leftmost depth. Day 43.

---
+++ October 2025/Week 3/10-13-2025/CountCompleteTreeNodes.cpp
@@ -0,0 +1,70 @@
+// 222. Count Complete Tree Nodes
+// https://leetcode.com/problems/count-complete-tree-nodes/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (root == nullptr) return 0;
+        if (root->left == nullptr) return 1;
+
+        int d = findLeftDepth(root);
+        cout << d << endl;
+
+        int left  = 1 << d;
+        int right = (1 << (d + 1)) - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // find if mid exists in the last level
+            // Start from the bit just below the MSB (skip the root’s implicit '1')
+            int mask = 1 << (d - 1);
+
+            TreeNode* curr = root;
+
+            cout << endl << "left: " << left << "\nright: " << right << "\n\n";
+            cout << "mid: " << mid << "\n";
+
+            while (mask && curr) {
+                cout << "curr node value: " << curr->val << "\n";
+                if (mid & mask) 
+                    curr = curr->right;    // bit 1 -> go right
+                else             
+                    curr = curr->left;     // bit 0 -> go left
+                
+                mask >>= 1;
+            }
+
+            if (curr == nullptr) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return right;
+    }
+
+    int findLeftDepth(TreeNode* curr) {
+        if (curr == nullptr)
+            return -1;
+        return 1+findLeftDepth(curr->left);
+    }
+};
