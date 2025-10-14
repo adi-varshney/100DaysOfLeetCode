932d12f2c87b1d4295660efe8c52f28661e623fb
Hypothetical <adi.varshney09@gmail.com>
2025-10-14 07:11:19 -0500

    Day 44 Code
    Implemented the invertTree function in C++ to recursively invert a binary tree, following LeetCode problem 226 - Day 44

---
+++ October 2025/Week 3/10-14-2025/InvertBinaryTree.cpp
@@ -0,0 +1,31 @@
+// 226. Invert Binary Tree
+// https://leetcode.com/problems/invert-binary-tree/
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+   TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr || root->left == nullptr && root->right == nullptr)
+            return root;
+        
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+    }
+};
