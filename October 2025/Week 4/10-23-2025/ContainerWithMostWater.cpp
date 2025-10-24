82803e052ddca7d1c6776a1aac9e93469868b2fe
Hypothetical <adi.varshney09@gmail.com>
2025-10-23 23:12:14 -0500

    Day 53 Code
    Implements 2 two-pointer approaches for LeetCode problem 11, Container With Most Water. Includes an original and a slightly optimized version to compute the maximum area between vertical lines - Day 53

---
+++ October 2025/Week 4/10-23-2025/ContainerWithMostWater.cpp
@@ -0,0 +1,65 @@
+// 11. Container With Most Water
+// https://leetcode.com/problems/container-with-most-water/
+
+#include <vector>
+using namespace std;
+
+// Solution 1, my original two-pointer approach
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int left = 0;
+        int right = height.size() - 1;
+        int ans = min(height[left], height[right]) * (right - left);
+
+        while (left < right)
+        {
+            if (height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+
+            ans = max(min(height[left], height[right]) * (right - left), ans);
+        }
+
+        return ans; // return area of rectangle between lowest/min high and distance between values
+    }
+};
+
+// Solution 2, slightly more optimized two-pointer approach
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int left = 0;
+        int right = height.size() - 1;
+        int ans = min(height[left], height[right]) * (right - left);
+
+        while (left < right)
+        {
+            if (height[left] < height[right])
+            {
+                int prev = height[left];
+                while (left < right && height[left] <= prev)
+                    ++left; // skip non-improving left heights
+            }
+            else
+            {
+                int prev = height[right];
+                while (left < right && height[right] <= prev)
+                    --right; // skip non-improving right heights
+            }
+
+            ans = max(min(height[left], height[right]) * (right - left), ans);
+        }
+
+        return ans; // return area of rectangle between lowest/min high and distance between values
+    }
+};
